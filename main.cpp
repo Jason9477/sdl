dcc561fa0f31aa1a69654cf9db9b8768ee65f6f5
Jason9477 <allen0981335260@gmail.com>
2023-12-05 14:56:44 +0800

    2p

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
+//#include <SDL2/SDL.h>
+//#include <SDL2/SDL_image.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-//#include<SDL.h>
-//#include <SDL_image.h>
+//#define nullptr NULL
 #include <iostream>
 
 const int SCREEN_WIDTH = 800;
@@ -15,8 +16,7 @@ int ballX, ballY;
 int ballSpeedX = 5;
 int ballSpeedY = 5;
 
-int player1X, player1Y;
-int player2X, player2Y;
+int player1X, player2X;
 
 SDL_Window* window;
 SDL_Renderer* renderer;
@@ -28,7 +28,7 @@ bool initSDL() {
         return false;
     }
 
-    window = SDL_CreateWindow("Pikachu Volleyball Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    window = SDL_CreateWindow("1D Pikachu Volleyball Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
     if (window == nullptr) {
         std::cerr << "Window could not be created! SDL_Error: " << SDL_GetError() << std::endl;
         return false;
@@ -89,10 +89,7 @@ void resetGame() {
     ballY = SCREEN_HEIGHT / 2 - BALL_SIZE / 2;
 
     player1X = SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2;
-    player1Y = SCREEN_HEIGHT - PADDLE_HEIGHT;
-
     player2X = SCREEN_WIDTH / 2 - PADDLE_WIDTH / 2;
-    player2Y = 0;
 }
 void update() {
     ballX += ballSpeedX;
@@ -102,24 +99,24 @@ void update() {
         ballSpeedX = -ballSpeedX;
     }
 
-    if (ballY < 0) {
+    if (ballY + BALL_SIZE > SCREEN_HEIGHT || ballY < 0) {
         ballSpeedY = -ballSpeedY;
     }
 
-    if (ballY + BALL_SIZE > player1Y && ballX > player1X && ballX < player1X + PADDLE_WIDTH) {
+    // Ball collision with paddles
+    if (ballY + BALL_SIZE > SCREEN_HEIGHT - PADDLE_HEIGHT && ballX > player1X && ballX < player1X + PADDLE_WIDTH) {
         ballSpeedY = -ballSpeedY;
     }
 
-    if (ballY < player2Y + PADDLE_HEIGHT && ballX > player2X && ballX < player2X + PADDLE_WIDTH) {
+    if (ballY < PADDLE_HEIGHT && ballX > player2X && ballX < player2X + PADDLE_WIDTH) {
         ballSpeedY = -ballSpeedY;
     }
 
+    // Check if the ball goes out of bounds
     if (ballY + BALL_SIZE > SCREEN_HEIGHT) {
-        // Player 1 scores
         std::cout << "Player 1 scores!" << std::endl;
         resetGame();
     } else if (ballY < 0) {
-        // Player 2 scores
         std::cout << "Player 2 scores!" << std::endl;
         resetGame();
     }
@@ -133,14 +130,16 @@ void render() {
     SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
     SDL_RenderFillRect(renderer, &ballRect);
 
-    SDL_Rect player1Rect = { player1X, player1Y, PADDLE_WIDTH, PADDLE_HEIGHT };
-    SDL_Rect player2Rect = { player2X, player2Y, PADDLE_WIDTH, PADDLE_HEIGHT };
+    SDL_Rect player1Rect = { player1X, SCREEN_HEIGHT - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT };
+    SDL_Rect player2Rect = { player2X, 0, PADDLE_WIDTH, PADDLE_HEIGHT };
     SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255);
     SDL_RenderFillRect(renderer, &player1Rect);
     SDL_RenderFillRect(renderer, &player2Rect);
 
-    SDL_Rect pikachuRect = { SCREEN_WIDTH / 2 - 50, SCREEN_HEIGHT / 2 - 50, 100, 100 };
-    SDL_RenderCopy(renderer, pikachuTexture, nullptr, &pikachuRect);
+    SDL_Rect pikachuRect1 = { player1X, SCREEN_HEIGHT - PADDLE_HEIGHT, PADDLE_WIDTH, PADDLE_HEIGHT };
+    SDL_Rect pikachuRect2 = { player2X, 0, PADDLE_WIDTH, PADDLE_HEIGHT };
+    SDL_RenderCopy(renderer, pikachuTexture, nullptr, &pikachuRect1);
+    SDL_RenderCopy(renderer, pikachuTexture, nullptr, &pikachuRect2);
 
     SDL_RenderPresent(renderer);
 }
